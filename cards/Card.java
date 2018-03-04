12aff424aa1a7447c9f5181e18207145434ecdb2
Serjes <serjes@yandex.ru>
2018-03-05 00:29:16 +0300

    Transfer balance from Card to Account

--- cards/Card.java
+++ cards/Card.java
@@ -7,29 +7,29 @@ import java.math.BigDecimal;
 public abstract class Card {
     private Account account;
 
-    private BigDecimal balance;
-
-    public Card() {
-        balance = new BigDecimal(1000.0);
-    }
-
-    public void setBalance(BigDecimal balance) {
-        this.balance = balance;
-    }
-
-    public BigDecimal getBalance() {
-        return balance;
-    }
-
-    public void withdraw(BigDecimal amount){
-        //BigDecimal tmpBalance =
-        //if (balance > amount)
-        if (balance.compareTo(amount) < 0) {
-            System.out.println("Не достаточно средств на карте");
-            //balance = balance.subtract(amount);
-            return;
-        }
-        balance = balance.subtract(amount);
-    }
+//    private BigDecimal balance;
+//
+//    public Card() {
+//        balance = new BigDecimal(1000.0);
+//    }
+//
+//    public void setBalance(BigDecimal balance) {
+//        this.balance = balance;
+//    }
+//
+//    public BigDecimal getBalance() {
+//        return balance;
+//    }
+//
+//    public void withdraw(BigDecimal amount){
+//        //BigDecimal tmpBalance =
+//        //if (balance > amount)
+//        if (balance.compareTo(amount) < 0) {
+//            System.out.println("Не достаточно средств на карте");
+//            //balance = balance.subtract(amount);
+//            return;
+//        }
+//        balance = balance.subtract(amount);
+//    }
 
 }
