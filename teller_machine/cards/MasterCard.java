8c7568c82883361c837cee5eb7293331b21b7efd
Serjes <serjes@yandex.ru>
2018-04-19 23:58:26 +0300

    Remove old directory

--- teller_machine/cards/MasterCard.java
+++
@@ -1,7 +0,0 @@
-package teller_machine.cards;
-
-public class MasterCard extends Card {
-    public MasterCard() {
-        description = "MasterCard";
-    }
-}
--- teller_machine/cards/VisaCard.java
+++
@@ -1,7 +0,0 @@
-package teller_machine.cards;
-
-public class VisaCard extends Card {
-    public VisaCard() {
-        description = "VisaCard";
-    }
-}
